22d314e8e381b27fa10b22680888e73df7038245
Serkan <serkan@basefy.com>
2020-05-03 16:54:57 +0300

    add download image feature

--- app/src/main/java/com/hsmnzaydn/waaperhd/ui/image_detail/ImageDetailFragment.kt
+++ app/src/main/java/com/hsmnzaydn/waaperhd/ui/image_detail/ImageDetailFragment.kt
@@ -1,23 +1,15 @@
 package com.hsmnzaydn.waaperhd.ui.image_detail
 
 import android.Manifest
-import android.app.WallpaperManager
 import android.os.Bundle
-import android.util.Log
 import com.basefy.base_mvp.BaseFragment
+import com.basefy.core_utility.CoreCommonUtils
 import com.basefy.core_utility.CoreImageloaderUtility
+import com.hsmnzaydn.waaperhd.R
 import com.hsmnzaydn.waaperhd.databinding.FragmentImageDetailBinding
 import com.hsmnzaydn.waaperhd.ui.controller
 import com.hsmnzaydn.waaperhd.utility.BundleConstant
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.MultiplePermissionsReport
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionDeniedResponse
-import com.karumi.dexter.listener.PermissionGrantedResponse
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener
-import com.karumi.dexter.listener.single.PermissionListener
-import kotlinx.android.synthetic.main.fragment_image_detail.*
+import com.tbruyelle.rxpermissions2.RxPermissions
 import javax.inject.Inject
 
 
@@ -25,12 +17,12 @@ class ImageDetailFragment : BaseFragment<FragmentImageDetailBinding>(), ImageDet
 
     @Inject
     lateinit var presenter: ImageDetailContract.Presenter<ImageDetailContract.View>
-
+    lateinit var rxPermissions: RxPermissions
 
     override fun initUI() {
         binding = FragmentImageDetailBinding.inflate(layoutInflater)
         presenter.onAttach(this)
-
+        rxPermissions = RxPermissions(this)
 
         presenter.getImage(arguments?.getString(BundleConstant.IMAGE_ID_BUNDLE))
         binding?.fragmentImageDetailToolbar!!.onClickBackIcon(controller)
@@ -41,6 +33,7 @@ class ImageDetailFragment : BaseFragment<FragmentImageDetailBinding>(), ImageDet
             binding!!.fragmentImageDetailProgressbar
         )
 
+
     }
 
     override fun setImageData(image: com.hsmnzaydn.waaperhd.image.domain.entities.Image.ImageDetail?) {
@@ -50,25 +43,62 @@ class ImageDetailFragment : BaseFragment<FragmentImageDetailBinding>(), ImageDet
             binding!!.fragmentImageDetailProgressbar
         )
 
-        fragment_image_detail_floatActionButton.setOnClickListener {
 
+        binding!!.fragmentImageDetailDownloadButton.setOnClickListener {
+            showLoading()
             CoreImageloaderUtility.getImageBitmap(
                 activity!!,
                 image!!.imagePath!!,
                 fileCallback = {
-                    val wallpaperManager =
-                        WallpaperManager.getInstance(activity)
-                    wallpaperManager.setBitmap(it)
+                    if (rxPermissions.isGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                        CoreCommonUtils.saveImage(
+                            activity!!,
+                            it,
+                            getString(R.string.app_name),
+                            saveImageCallback = {
+                                hideLoading()
+                                showInformation(getString(R.string.image_detail_downloaded_image))
+                            })
+                    } else {
+                        hideLoading()
+                        rxPermissions
+                            .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            .subscribe { granted: Boolean ->
+                                if (granted) {
+                                    showLoading()
+                                    CoreCommonUtils.saveImage(
+                                        activity!!,
+                                        it,
+                                        getString(R.string.app_name),
+                                        saveImageCallback = {
+                                            hideLoading()
+                                            showInformation(getString(R.string.image_detail_downloaded_image))
+                                        })
+                                } else {
+                                    showError(getString(R.string.warning_permission))
+                                }
+                            }
+                    }
+
+
                 })
 
+
+        }
+
+        binding!!.fragmentImageDetailSetButton.setOnClickListener {
+            /*    val wallpaperManager =
+                    WallpaperManager.getInstance(activity)
+                wallpaperManager.setBitmap(it)*/
         }
     }
 
+
     companion object {
-        fun getImageDetailInstance(imageId: String,imageUrl:String): ImageDetailFragment {
+        fun getImageDetailInstance(imageId: String, imageUrl: String): ImageDetailFragment {
             var bundle = Bundle()
             bundle.putString(BundleConstant.IMAGE_ID_BUNDLE, imageId)
-            bundle.putString(BundleConstant.IMAGE_URL_BUNDLE,imageUrl)
+            bundle.putString(BundleConstant.IMAGE_URL_BUNDLE, imageUrl)
             var fragment = ImageDetailFragment()
             fragment.arguments = bundle
             return fragment
--- core_utility/src/main/java/com/basefy/core_utility/CoreCommonUtils.kt
+++ core_utility/src/main/java/com/basefy/core_utility/CoreCommonUtils.kt
@@ -2,15 +2,24 @@ package com.basefy.core_utility
 
 import android.app.Activity
 import android.app.ProgressDialog
+import android.content.ContentValues
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.DisplayMetrics
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.core.app.ActivityCompat
 import android.provider.Settings;
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.util.*
 
 
 class CoreCommonUtils {
@@ -43,6 +52,59 @@ class CoreCommonUtils {
                 context.contentResolver,
                 Settings.Secure.ANDROID_ID
             )
+        }
+
+
+         fun saveImage(activity: Activity,bitmap: Bitmap, folderName: String,saveImageCallback :() -> Unit) {
+
+            if (android.os.Build.VERSION.SDK_INT >= 29) {
+                val values = contentValues()
+                values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + folderName)
+                values.put(MediaStore.Images.Media.IS_PENDING, true)
+                val uri: Uri? =
+                    activity.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+                if (uri != null) {
+                    saveImageToStream(bitmap, activity.contentResolver.openOutputStream(uri))
+                    values.put(MediaStore.Images.Media.IS_PENDING, false)
+                    activity.contentResolver.update(uri, values, null, null)
+                }
+            } else {
+                var filename = UUID.randomUUID()
+                var file =
+                    File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath().toString() + File.separator + folderName)
+                if (!file.exists()) {
+                    file.mkdir()
+                }
+                file =
+                    File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath().toString() + File.separator + folderName + File.separator + filename + ".jpg")
+
+                saveImageToStream(bitmap, FileOutputStream(file))
+                if (file.absolutePath != null) {
+                    val values = contentValues()
+                    values.put(MediaStore.Images.Media.DATA, file.absolutePath)
+                    activity.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+                }
+            }
+             saveImageCallback()
+        }
+
+        private fun contentValues(): ContentValues {
+            val values = ContentValues()
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+            values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+            return values
+        }
+
+        private fun saveImageToStream(bitmap: Bitmap, outputStream: OutputStream?) {
+            if (outputStream != null) {
+                try {
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                    outputStream.close()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
         }
     }
 
